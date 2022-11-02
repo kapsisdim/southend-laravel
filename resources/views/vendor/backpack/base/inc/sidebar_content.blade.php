97e4669d7c11d5e142e16e0996dda37dc5cfc617
Dimitris Kapsis <dkapsis@steficon.gr>
2022-11-02 20:22:56 +0200

    kapsis- categories and collections

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,4 +1,6 @@
 {{-- This file is used to store sidebar items, inside the Backpack admin panel --}}
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('menu-items') }}"><i class="la la-bars"></i></i> Menu</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('categories') }}"><i class="la la-tags nav-icon"></i> Categories</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('collections') }}"><i class="la la-tags nav-icon"></i> Collections</a></li>
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('socials') }}"><i class="la la-share"></i></i> Socials</a></li>
