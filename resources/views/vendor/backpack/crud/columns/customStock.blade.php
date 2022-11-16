42fe004122d733caf394f5e33dee162182c84693
Dimitris Kapsis <dkapsis@steficon.gr>
2022-11-16 22:54:25 +0200

    kapsis categories and collections

--- resources/views/vendor/backpack/crud/columns/customStock.blade.php
+++ resources/views/vendor/backpack/crud/columns/customStock.blade.php
@@ -3,4 +3,12 @@
 <?php foreach($entry->sizes as $size): ?>
     <?php $stock = $stock + $size->pivot->amount; ?>
 <?php endforeach; ?>
-<span><?=$stock?></span>
+
+<?php if ($stock == 0): ?>
+    <span class="badge badge-danger">{{ $stock }}</span>
+<?php elseif($stock < 10): ?>
+    <span class="badge badge-warning">{{ $stock }}</span>
+<?php else: ?>
+    <span class="badge badge-primary">{{ $stock }}</span>
+<?php endif; ?>
+
