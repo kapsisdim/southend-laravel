abd3e31ce75e53c552ca85f53afa8182061f0d13
Dimitris Kapsis <dkapsis@steficon.gr>
2022-12-01 17:59:27 +0200

    hp controller

--- app/Http/Controllers/HomePageController.php
+++ app/Http/Controllers/HomePageController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\Social;
 use App\Models\MenuItem;
+use App\Models\FrontPage;
+use App\Models\Info;
+use App\Models\Category;
 
 class HomePageController extends Controller
 {
@@ -13,7 +16,20 @@ class HomePageController extends Controller
     public function index()
     {
         $socials = Social::orderBy('lft', 'asc')->get();
+        $menuItems = MenuItem::orderBy('lft', 'asc')->get();
+        $mainMenuItems = MenuItem::orderBy('lft', 'asc')->where('main_menu', 1)->get();
+        $frontPage = FrontPage::first();
+        $info = Info::first();
+        $categories = Category::get();
+
         return view('index', [
+            'socials' => $socials,
+            'frontPage' => $frontPage,
+            'menuItems' => $menuItems,
+            'mainMenuItems' => $mainMenuItems,
+            'info' => $info,
+            'categories' => $categories,
+
         ]);
     }
 }
